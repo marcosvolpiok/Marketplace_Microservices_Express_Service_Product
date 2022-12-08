20174c07d084f868fcfa4f6f63b0f321c8f4898b
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-08 07:26:49 -0300

    Refactor

--- src/dependencies/index.js
+++ src/dependencies/index.js
@@ -5,11 +5,6 @@ const { cacheHelper } = require('../helpers/cacheHelper');
 //const clientOb = createClient.getClient();
 const cacheHelperOb = new cacheHelper(createClient);
 
-
-
-const shopController = require('../controllers/shopController');
-const productController = require('../controllers/productController');
-
 const shopRepository = require('../repository/shopRepository');
 const productRepository = require('../repository/productRepository');
 
@@ -21,13 +16,11 @@ const {Shop, Product, Image, Sequelize, sequelize} = require('../models');
 
 const shopRepositoryOb=new shopRepository(Shop, Sequelize, sequelize, cacheHelperOb);
 const shopServiceOb = new shopService(shopRepositoryOb);
-const shopControllerOb = new shopController(shopServiceOb);
 
 const productRepositoryOb=new productRepository(Product, Image, Sequelize, sequelize, cacheHelperOb);
 const productServiceOb = new productService(productRepositoryOb);
-const productControllerOb = new productController(productServiceOb);
 
 module.exports = {
-    Sequelize, sequelize, shopControllerOb, productControllerOb,
+    Sequelize, sequelize,
     shopServiceOb, productServiceOb
 };
